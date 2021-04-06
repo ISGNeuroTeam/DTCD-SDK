4714aa5df069fd58e797a785225a005b8474b823
Konstantin <konstantin@isgneuro.com>
2021-04-06 12:40:38 +0300

    1. Hotfixed: getInstance method.

--- DTCD-SDK/PluginClasses/AbstractPlugin.js
+++ DTCD-SDK/PluginClasses/AbstractPlugin.js
@@ -7,96 +7,95 @@
  * @property {String[]} requirements
  */
 export class AbstractPlugin {
-	/**
-	 * Static method of AbstractPlugin class which need to reload!
-	 * @static
-	 * @returns {PluginMeta}
-	 * @return {String} meta.title
-	 * @return {String} meta.name
-	 * @return {String[]} meta.actions
-	 * @return {String[]} meta.events
-	 * @return {String[]} meta.dependencies
-	 */
-	static getRegistrationMeta() {
-		throw new Error('Implement the getRegistrationMeta static method!');
-	}
+  /**
+   * Static method of AbstractPlugin class which need to reload!
+   * @static
+   * @returns {PluginMeta}
+   * @return {String} meta.title
+   * @return {String} meta.name
+   * @return {String[]} meta.actions
+   * @return {String[]} meta.events
+   * @return {String[]} meta.dependencies
+   */
+  static getRegistrationMeta() {
+    throw new Error('Implement the getRegistrationMeta static method!');
+  }
 
-	/**
-	 * Getting module from dependencies
-	 * @method
-	 * @param {String} name
-	 * @returns {Object[]}
-	 */
-	getDependence(name) {
-		return Application.getDependence(name);
-	}
+  /**
+   * Getting module from dependencies
+   * @method
+   * @param {String} name
+   * @returns {Object[]}
+   */
+  getDependence(name) {
+    return Application.getDependence(name);
+  }
 
-	/**
-	 * Getting all extensions for plugin by name
-	 * @method
-	 * @param {String} name
-	 * @return {Object[]}
-	 */
-	getExtensions(name) {
-		return Application.getExtensions(name);
-	}
+  /**
+   * Getting all extensions for plugin by name
+   * @method
+   * @param {String} name
+   * @return {Object[]}
+   */
+  getExtensions(name) {
+    return Application.getExtensions(name);
+  }
 
-	/**
-	 * Getting list of all awailable panels
-	 * @method
-	 * @return {Object[]}
-	 */
-	getPanels() {
-		return Application.getPanels();
-	}
+  /**
+   * Getting list of all awailable panels
+   * @method
+   * @return {Object[]}
+   */
+  getPanels() {
+    return Application.getPanels();
+  }
 
-	/**
-	 * Getting system by name
-	 * @method
-	 * @param {String} name
-	 * @return {Object}
-	 */
-	getSystem(name) {
-		return Application.getSystem(name);
-	}
+  /**
+   * Getting system by name
+   * @method
+   * @param {String} name
+   * @return {Object}
+   */
+  getSystem(name) {
+    return Application.getSystem(name);
+  }
 
-	/**
-	 * Installing plugin by name
-	 * @method
-	 * @param {String} name
-	 */
-	installPlugin(name, ...args) {
-		return Application.installPlugin(name, ...args);
-	}
+  /**
+   * Installing plugin by name
+   * @method
+   * @param {String} name
+   */
+  installPlugin(name, ...args) {
+    return Application.installPlugin(name, ...args);
+  }
 
-	/**
-	 * Uninstall plugin from Application by instance
-	 * @method
-	 * @param {Object} instance
-	 * @returns {Boolean}
-	 */
-	uninstallPluginByInstance(instance) {
-		return Application.uninstallPluginByInstance(instance);
-	}
+  /**
+   * Uninstall plugin from Application by instance
+   * @method
+   * @param {Object} instance
+   * @returns {Boolean}
+   */
+  uninstallPluginByInstance(instance) {
+    return Application.uninstallPluginByInstance(instance);
+  }
 
-	/**
-	 * Uninstall plugin from Application by unique identifier
-	 * @method
-	 * @param {String} guid Unique identifier of the instance to be uninstalled
-	 * @returns {Boolean}
-	 */
-	uninstallPluginByGUID(guid) {
-		return Application.uninstallPluginByGUID(guid);
-	}
+  /**
+   * Uninstall plugin from Application by unique identifier
+   * @method
+   * @param {String} guid Unique identifier of the instance to be uninstalled
+   * @returns {Boolean}
+   */
+  uninstallPluginByGUID(guid) {
+    return Application.uninstallPluginByGUID(guid);
+  }
 
-	/**
-	 * Getting instance by guid
-	 * @method
-	 * @param {String} guid
-	 * @returns {Object}
-	 */
-	getInstance(guid) {
-		this.instance.getInstance(guid);
-		return this.instance.getInstance(guid);
-	}
+  /**
+   * Getting instance by guid
+   * @method
+   * @param {String} guid
+   * @returns {Object}
+   */
+  getInstance(guid) {
+    return Application.getInstance(guid);
+  }
 }

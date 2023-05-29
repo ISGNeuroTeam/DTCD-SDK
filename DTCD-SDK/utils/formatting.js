c23822d55becf8f9f0020ed0c8e032a5b8a717d1
romankuramshin <rkuramshin@isgneuro.com>
2023-05-29 23:31:03 +0400

    added digits after comma in formatNumber fucntion

--- DTCD-SDK/utils/formatting.js
+++ DTCD-SDK/utils/formatting.js
@@ -4,21 +4,33 @@
  * @param {String} [targetLocale] Locale to format. Example: 'no', 'auto', 'ru-RU', etc. Default: locale from localStorage (key - 'settings.numberFormat').
  * @returns {String} Formatted number or 'number' parameter.
  */
-function formatNumberToLocale(number, targetLocale) {
+function formatNumberToLocale(number, targetLocale, maximumFractionDigits) {
   if (typeof number != 'number') return number;
 
   if (!targetLocale) {
     targetLocale = window.localStorage.getItem('settings.numberFormat') || 'no';
   }
+  
+  if (typeof maximumFractionDigits === 'number' && maximumFractionDigits > -1 && maximumFractionDigits < 11) {
+    number = truncNumber(number, maximumFractionDigits)
+  } else {
+    maximumFractionDigits = parseInt(window.localStorage.getItem('settings.signsAfterComma')) ?? -1;
+    number = maximumFractionDigits !== -1 ? truncNumber(number, maximumFractionDigits) : number;
+  }
 
   if (targetLocale == 'no') return number;
 
   if (targetLocale == 'auto') {
-    return number.toLocaleString();
+    return number.toLocaleString(undefined, options);
   } else {
-    return number.toLocaleString(targetLocale);
+    return number.toLocaleString(targetLocale, options);
   }
 };
+
+
+function truncNumber(number, maximumFractionDigits) {
+  return Math.trunc(number*Math.pow(10, maximumFractionDigits))/Math.pow(10, maximumFractionDigits)
+}
 
 export {
   formatNumberToLocale,
